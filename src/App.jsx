f1474a828a02ab8788de3291202d309ae568656b
Sophia Cherkaoui <soph789opr@gmail.com>
2025-07-19 11:20:02 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
-const App = () => {
-  return <div className="text-6xl text-green-500">Hiii</div>;
-};
+import { Routes, Route } from 'react-router-dom'
+import Splash_page from './Splash_page'
+import Login_page from './Login_page'
+import Homepage from './Homepage'
 
-export default App;
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Splash_page/>} />
+      <Route path="/login" element={<Login_page/>} />
+      <Route path="/home" element={<Homepage/>} />
+    </Routes>
+  )
+}
+
+export default App
+
